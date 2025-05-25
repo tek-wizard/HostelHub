a333114cb40376b7f2f4b6b4adaa32a7bdc9c05d
Pratham Onkar Singh <prathamaj09@gmail.com>
2025-05-25 11:27:01 +0530

    port error on render

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const API_BASE_URL = "https://hostelhub-mnr8.onrender.com" || "http://localhost:8000";
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 const api = axios.create({
     baseURL: API_BASE_URL,
