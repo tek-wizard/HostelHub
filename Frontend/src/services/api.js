4abbc61fbfc59593379b8c4159c633d37a98844c
Pratham Onkar Singh <prathamaj09@gmail.com>
2025-05-25 11:08:13 +0530

    debugging deployment

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = "https://hostelhub-mnr8.onrender.com" || "http://localhost:8000";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
